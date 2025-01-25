758e224058c6c77330b68c189a45bd46e24213d7
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-25 10:51:34 +0100

    Code en Francais

--- Pokemon/shop.c
+++ Pokemon/shop.c
@@ -2,12 +2,12 @@
 #include "shop.h"
 
 void shop(Player *player) {
-    printf("\nWelcome to the Shop!\n");
-    printf("1. Buy Potion (100 Supcoins)\n");
-    printf("2. Buy Super Potion (300 Supcoins)\n");
-    printf("3. Buy Rare Candy (700 Supcoins)\n");
-    printf("4. Exit Shop\n");
-    printf("Enter your choice: ");
+    printf("\n Bienvenue dans la magasin!\n");
+    printf("1. Acheter une potion (100 Supcoins)\n");
+    printf("2. Acheter une super potion (300 Supcoins)\n");
+    printf("3. Acheter un bonbon rare (700 Supcoins)\n");
+    printf("4. Quitter le magasin\n");
+    printf("Entrez votre choix: ");
 
     int choice;
     scanf("%d", &choice);
@@ -17,35 +17,33 @@ void shop(Player *player) {
             if (player->supcoins >= 100) {
                 player->supcoins -= 100;
                 player->items[0]++;
-                printf("You bought a Potion!\n");
+                printf("Vous achetez une potion!\n");
             } else {
-                printf("Not enough Supcoins!\n");
+                printf("Pas assez de Supcoins!\n");
             }
             break;
         case 2:
             if (player->supcoins >= 300) {
                 player->supcoins -= 300;
                 player->items[1]++;
-                printf("You bought a Super Potion!\n");
+                printf("Vous achetez une Super Potion!\n");
             } else {
-                printf("Not enough Supcoins!\n");
+                printf("Pas assez de Supcoins!\n");
             }
             break;
         case 3:
             if (player->supcoins >= 700) {
                 player->supcoins -= 700;
                 player->items[2]++;
-                printf("You bought a Rare Candy!\n");
+                printf("Vous achetez un bonbon rare!\n");
             } else {
-                printf("Not enough Supcoins!\n");
+                printf("Pas assez de Supcoins!\n");
             }
             break;
         case 4:
-            printf("Exiting shop.\n");
+            printf("Vous quittez le magasin.\n");
             return;
         default:
-            printf("Invalid choice. Try again.\n");
+            printf("Choix invalide. Reessayer.\n");
     }
 }
-
-
